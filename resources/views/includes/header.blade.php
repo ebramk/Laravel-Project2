89717946513ab309498ea4f4302e7c7a04e2c17b
ebramk <er86@njit.edu>
2019-04-01 11:17:30 -0400

    href fixed

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -14,7 +14,7 @@
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link" href="includes/pages/contact">Contact Us</a>
+                        <a class="nav-link" href="includes/pages/contact.blade.php">Contact Us</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="/login">Login</a>
